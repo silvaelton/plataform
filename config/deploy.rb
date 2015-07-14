84ee0cfb2380bd7605635b3d10d4e08f0f95c2e2
Elton Silva <elton.chrls@gmail.com>
2015-07-14 14:11:04 -0300

    correçẽos

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,9 +11,10 @@ set :deploy_to, '/home/deploy/plataform'
 set :repository, 'https://github.com/eltonchrls/plataform.git'
 set :branch, 'master'
 
-set :unicorn_config, 'unicorn.rb'
+set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
 
-set :shared_paths, ['config/database.yml', 'log', 'tmp/sockets', 'tmp/pids']
+
+set :shared_paths, ['config/database.yml', 'log']
 
 task :environment do
   invoke :'rvm:use[ruby-2.2.1]'
@@ -32,7 +33,7 @@ task :logs do
   queue 'tail -f /var/log/nginx/error.log'
 end
 
-task :setup  => :environment do 
+task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/shared/log"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
 
@@ -43,16 +44,20 @@ task :setup  => :environment do
   queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
 end
 
-task :deploy => :environment do 
-  deploy do 
-    invoke :'git:clone' 
-    invoke :'deploy:link_shared_paths' 
-    invoke :'bundle:install' 
-    invoke :'rails:db_migrate' 
-    invoke :'rails:assets_precompile' 
-    
-    to :launch do 
-      queue "touch #{deploy_to}/tmp/restart.txt" 
-    end 
-  end 
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  deploy do
+    # Put things that will set up an empty directory into a fully set-up
+    # instance of your project.
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
+    invoke :'deploy:cleanup'
+
+    to :launch do
+      invoke :'unicorn:restart'
+    end
+  end
 end
