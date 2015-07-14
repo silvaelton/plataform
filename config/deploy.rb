adf190f932082c50aefc4d671858b507e923e3b3
Elton Silva <elton.chrls@gmail.com>
2015-07-14 14:14:18 -0300

    correçẽos

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,8 +40,7 @@ task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/shared/config"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
 
-  queue! %[touch "#{deploy_to}/shared/config/database.yml"]
-  queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
+  queue  %[cp #{deploy_to}/current/config/database.sample.yml shared/config/database.yml]
 end
 
 desc "Deploys the current version to the server."
